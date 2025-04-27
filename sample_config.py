ce18ac438969289cbb3892dd0d247430e1817898
rasa9521 <rsrasasingh@gmail.com>
2025-04-27 12:04:25 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,10 @@
 import os
 
-api_id = 3748059
-api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
-bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
-sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
-owner_users = [int(num) for num in osowner_users.split(",")]
+api_id = os.environ.get("API_ID", "27900743)
+api_hash = os.environ.get("API_HASH", "febb06ea8d41420e60b29140dcee902fc")
+bot_token = os.environ.get("BOT_TOKEN" "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
+auth_users = os.environ.get("AUTH_USERS", "7804396225,7804396225")
+sudo_users = [int(num) for num in auth_users.split("7804396225")]
+osowner_users = os.environ.get("OWNER_USERS", "7804396225")
+owner_users = [int(num) for num in osowner_users.split("7804396225")]
+
