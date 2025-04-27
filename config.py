a1d1af461f41ff28f2fca778c85b3aff87411fc8
rasa9521 <rsrasasingh@gmail.com>
2025-04-27 12:09:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = os.environ.get("API_ID", "27900743)
+api_id = os.environ.get("API_ID", "27900743")
 api_hash = os.environ.get("API_HASH", "febb06ea8d41420e60b29140dcee902fc")
 bot_token = os.environ.get("BOT_TOKEN" "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
 auth_users = os.environ.get("AUTH_USERS", "7804396225,7804396225")
