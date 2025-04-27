44532017545c102607ab1e8a4f4299fc3f7b3306
rasa9521 <rsrasasingh@gmail.com>
2025-04-27 12:10:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = os.environ.get("API_ID", "27900743")
-api_hash = os.environ.get("API_HASH", "febb06ea8d41420e60b29140dcee902fc")
+api_hash = os.environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
 bot_token = os.environ.get("BOT_TOKEN" "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
 auth_users = os.environ.get("AUTH_USERS", "7804396225,7804396225")
 sudo_users = [int(num) for num in auth_users.split("7804396225")]
